58f0e23ad77371a51bbbad5f2580bc395304b854
infinite-cloud <54478207+infinite-cloud@users.noreply.github.com>
2020-03-20 11:55:56 +0300

    Refactor Renderer

--- raytracer/lib/main.cpp
+++ raytracer/lib/main.cpp
@@ -14,7 +14,7 @@
 
 int main(int argc, char *argv[])
 {
-    Renderer test(glm::uvec2(1024, 768), std::acos(-1.0f) / 2.0f, 5);
+    Renderer test(glm::uvec2(1280, 720), std::acos(-1.0f) / 2.0f, 5);
     Scene scene;
 
     auto ivory = std::make_shared<Material>(glm::vec3(0.4, 0.4, 0.3),
@@ -37,16 +37,6 @@ int main(int argc, char *argv[])
     scene.objects().push_back(std::unique_ptr<Plane>(
         new Plane(glm::normalize(glm::vec3(0, 1, 0)),
         glm::vec3(0, -3.5, 0), ivory)));
-    scene.objects().push_back(std::unique_ptr<Plane>(
-        new Plane(glm::normalize(glm::vec3(1, 0, 0)),
-        glm::vec3(-8, 0, 0), ivory)));
-    scene.objects().push_back(std::unique_ptr<Plane>(
-        new Plane(glm::normalize(glm::vec3(-1, 0, 0)),
-        glm::vec3(8, 0, 0), ivory)));
-    scene.objects().push_back(std::unique_ptr<Plane>(
-        new Plane(glm::normalize(glm::vec3(0, -1, 0)),
-        glm::vec3(0, 9, 0), ivory)));
-
 
     scene.point_lights().push_back(std::unique_ptr<PointLight>(
         new PointLight(glm::vec3(-20, 20, 20), 1.5f)));
@@ -54,8 +44,6 @@ int main(int argc, char *argv[])
         new PointLight(glm::vec3(30, 50, -25), 1.8f)));
     scene.point_lights().push_back(std::unique_ptr<PointLight>(
         new PointLight(glm::vec3(30, 20, 30), 1.7f)));
-    scene.point_lights().push_back(std::unique_ptr<PointLight>(
-        new PointLight(glm::vec3(1, 2.5, 1), 1.7f)));
 
     Image img = test.render(scene);
     img.save_bmp("test.bmp");
--- raytracer/lib/renderer.cpp
+++ raytracer/lib/renderer.cpp
@@ -113,3 +113,32 @@ glm::vec3 Renderer::render_ray(const Scene &scene, const Ray &ray,
 
     return color;
 }
+
+glm::vec3 Renderer::reflect(const glm::vec3 &indice, const glm::vec3 &normal)
+    const
+{
+    return indice - 2.0f * normal * glm::dot(indice, normal);
+}
+
+glm::vec3 Renderer::refract(const glm::vec3 &indice, const glm::vec3 &normal,
+    float refracive_index) const
+{
+    float cos_i = -std::max(-1.0f, std::min(1.0f, glm::dot(indice, normal)));
+    float eta_i = 1;
+    float eta_t = refracive_index;
+
+    glm::vec3 n = normal;
+
+    if (cos_i < 0)
+    {
+        cos_i = -cos_i;
+        std::swap(eta_i, eta_t);
+        n = -n;
+    }
+
+    float eta = eta_i / eta_t;
+    float k = 1 - eta * eta * (1 - cos_i * cos_i);
+
+    return (k < 0) ? glm::vec3(0) :
+        indice * eta + n * (eta * cos_i - std::sqrt(k));
+}
