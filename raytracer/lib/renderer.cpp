1167fdc85c2fdd0dca51f6f04c0d5fd18a2119b2
infinite-cloud <54478207+infinite-cloud@users.noreply.github.com>
2020-03-21 22:37:22 +0300

    Use proper PT parameters

--- raytracer/lib/renderer.cpp
+++ raytracer/lib/renderer.cpp
@@ -36,11 +36,10 @@ Image Renderer::render(const Scene &scene) const
     }
     else
     {
-        double fov_scale = 2 * std::tan(0.5d * _fov) / 4;
+        double fov_scale = 2 * std::tan(0.5d * _fov);
         glm::dvec3 cx = glm::dvec3(_size.x * fov_scale / _size.y, 0, 0);
         glm::dvec3 cy = glm::normalize(glm::cross(cx, glm::dvec3(0, 0, -1))) *
             fov_scale;
-        glm::dvec3 origin = glm::dvec3(50,52,295.6);
         glm::dvec3 color;
         glm::dvec3 r;
         auto f = [](double x) { return static_cast<int>((std::pow(
@@ -59,7 +58,7 @@ Image Renderer::render(const Scene &scene) const
                     {
                         r = glm::dvec3(0);
 
-                        for (size_t s = 0; s < 25; ++s)
+                        for (size_t s = 0; s < 20; ++s)
                         {
                             double r_1 = 2 * erand48();
                             double dx = (r_1 < 1) ? std::sqrt(r_1) - 1:
@@ -74,7 +73,7 @@ Image Renderer::render(const Scene &scene) const
                                 glm::dvec3(0, 0, -1);
 
                             r += render_path(scene,
-                                Ray(origin + d * 140.0d, glm::normalize(d)), 0) *
+                                Ray(d, glm::normalize(d)), 0) *
                                 (1.0d / 10);
                         }
 
